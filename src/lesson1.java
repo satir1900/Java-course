d245bb426be203ab86c8f819dfaee192a07ac2e8
prog <kuzmin.serg@gmail.com>
2023-11-28 21:32:54 +0300

    добавлен урок 1

---
+++ src/lesson1.java
@@ -0,0 +1,8 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Ghbdtn");
+
+    }
+}
