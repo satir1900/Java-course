4ebaf8fbf0fccb634c5858b977629454ce620c2a
prog <kuzmin.serg@gmail.com>
2023-11-29 21:03:14 +0300

    добавлено решение 2 задачи 1 урока

--- src/lesson1.java
+++ src/lesson1.java
@@ -2,6 +2,7 @@ public class lesson1 {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
+        String FIO = "Иванов Иван Иванович";
 
         System.out.println("Задача №1");
         // место для вашего кода
@@ -12,6 +13,7 @@ public class lesson1 {
         System.out.println("Значение а: " + a + ". Значение b: " + b);
 
         System.out.println("Задача №2");
-
+        System.out.println(FIO);
+        System.out.print(FIO);
     }
 }
