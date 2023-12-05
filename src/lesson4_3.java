2487cc03faa8cb7c6974e66d827c546761ae4e84
satir1900 <kuzmin.serg@gmail.com>
2023-12-05 21:08:11 +0300

    Урок № 4 решение задачи 3

--- src/lesson4_3.java
+++ src/lesson4_3.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
 
-public class lesson4_2 {
+public class lesson4_3 {
     public static void main(String[] args) {
+        System.out.print("Написать программу, которая принимает длину и ширину прямоугольника (2 целых числа). \nНарисовать в консоли заданный прямоугольник, используя “-“ и “|”. Углы прямоугольника обозначить символом “ “. \nКаждая единица длины должна обозначаться одним символом “-“, каждая единица ширины – символом “|“.\n");
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.print("Введите ширину прямоугольника: ");
         int a = in.nextInt();
-        int c = 0;
-        while (a > 0){
+        System.out.print("Введите длину прямоугольника: ");
+        int b = in.nextInt();
+        //int y;
 
-            c = c + a;
-            a--;
+        for (int x = 1; x <= a + 2; x++){
+            for (int y = 1; y <= b + 2; y++){
+
+                if (((x == 1) || (x == a + 2))
+                && ((y == 1) || (y == b + 2))){
+                    System.out.print(" ");
+                }
+                else if (((x == 1) || (x == a + 2))
+                        && ((y != 1) || (y != b + 2))) {
+                    System.out.print("-");
+                }
+                else if (((x != 1) && (x != a + 2))
+                        && ((y == 1) || (y == b + 2))) {
+                    System.out.print("|");
+                }
+                else {
+                    System.out.print(" ");
+                }
+                if (y == b + 2) {
+                    System.out.println();
+                }
+            }
         }
-
-        System.out.println("Result = " + c);
     }
 }
