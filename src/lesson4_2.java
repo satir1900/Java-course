0c64a3bd0db7f0973e2fa5d93fd92f7bdae43139
satir1900 <kuzmin.serg@gmail.com>
2023-12-05 21:13:27 +0300

    Урок № 4 решение задачи 2

--- src/lesson4_2.java
+++ src/lesson4_2.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class lesson4_1 {
+public class lesson4_2 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число:");
         int a = in.nextInt();
-        int c = 1;
+        int c = 0;
         while (a > 0){
 
-            c = c * a;
+            c = c + a;
             a--;
         }
 
